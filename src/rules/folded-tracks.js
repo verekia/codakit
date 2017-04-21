cd847d7e542e4a7354e1daa685136474c4851172
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2017-04-21 11:47:57 +0700

    Made folded-tracks rule aware of isException

--- src/rules/folded-tracks.js
+++ src/rules/folded-tracks.js
@@ -3,12 +3,12 @@
 import _ from 'lodash'
 import 'colors'
 
-import { getAllTracks } from '../utils'
+import { getAllTracks, isExceptionTrack } from '../utils'
 
 export default (abletonObj: Object) => {
   const errors = []
   getAllTracks(abletonObj).forEach((track) => {
-    if (track.DeviceChain.Mixer.ViewStateSesstionTrackWidth.Value !== '55') {
+    if (track.DeviceChain.Mixer.ViewStateSesstionTrackWidth.Value !== '55' && !isExceptionTrack(track)) {
       errors.push(`Adjust width of ${track.Name.EffectiveName.Value.green} in Session view`)
     }
     if (_.isArray(track.DeviceChain.AutomationLanes.AutomationLanes.AutomationLane)) {
@@ -16,12 +16,12 @@ export default (abletonObj: Object) => {
     } else {
       if (
         track.DeviceChain.AutomationLanes.AutomationLanes.AutomationLane.LaneHeight.Value !== '17' &&
-        track.DeviceChain.AutomationLanes.AutomationLanes.AutomationLane.LaneHeight.Value !== '34') {
+        track.DeviceChain.AutomationLanes.AutomationLanes.AutomationLane.LaneHeight.Value !== '34' && !isExceptionTrack(track)) {
         errors.push(`Fold default automation lane of ${track.Name.EffectiveName.Value.green}`)
       }
       if (
-        track.DeviceChain.AutomationLanes.AutomationLanes.AutomationLane.SelectedDevice.Value !== '0' ||
-        track.DeviceChain.AutomationLanes.AutomationLanes.AutomationLane.SelectedEnvelope.Value !== '0') {
+        (track.DeviceChain.AutomationLanes.AutomationLanes.AutomationLane.SelectedDevice.Value !== '0' ||
+        track.DeviceChain.AutomationLanes.AutomationLanes.AutomationLane.SelectedEnvelope.Value !== '0') && !isExceptionTrack(track)) {
         errors.push(`Select None for automation of ${track.Name.EffectiveName.Value.green}`)
       }
     }
