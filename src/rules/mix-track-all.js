d4c52ae1692eb43ae3e39cd763cc693ca54dc42e
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2017-04-22 10:45:36 +0700

    Add SC to mix-track-all

--- src/rules/mix-track-all.js
+++ src/rules/mix-track-all.js
@@ -6,13 +6,19 @@ import { getAllTracks, findTrackByName, isExceptionTrack } from '../utils'
 
 export default (abletonObj: Object) => {
   const errors = []
-  const mixTrack = findTrackByName(abletonObj, 'Mix')
-  const allButMixTrack = getAllTracks(abletonObj).filter(track => track.Name.EffectiveName.Value !== 'Mix')
+  const mixLabel = '(Mix)'
+  const scLabel = '(SC)'
+  const mixTrack = findTrackByName(abletonObj, mixLabel)
+  const scTrack = findTrackByName(abletonObj, scLabel)
+  const allButThoseTracks = getAllTracks(abletonObj).filter(track =>
+    track.Name.EffectiveName.Value !== mixLabel && track.Name.EffectiveName.Value !== scLabel)
   if (mixTrack) {
-    allButMixTrack.forEach((track) => {
-      if (track.DeviceChain.AudioOutputRouting.Target.Value !== `AudioOut/Track.${mixTrack.$.Id}/TrackIn` && track.TrackGroupId.Value === '-1' && !isExceptionTrack(track)) {
+    allButThoseTracks.forEach((track) => {
+      if (track.DeviceChain.AudioOutputRouting.Target.Value !== `AudioOut/Track.${mixTrack.$.Id}/TrackIn`
+        && track.DeviceChain.AudioOutputRouting.Target.Value !== `AudioOut/Track.${scTrack.$.Id}/TrackIn`
+        && track.TrackGroupId.Value === '-1' && !isExceptionTrack(track)) {
         // flow-disable-next-line
-        errors.push(`Set track ${track.Name.EffectiveName.Value.green}'s audio output to ${'Mix'.green}`)
+        errors.push(`Set track ${track.Name.EffectiveName.Value.green}'s audio output to ${mixLabel.green} or ${scLabel.green}`)
       }
     })
   }
